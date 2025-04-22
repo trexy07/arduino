21c291606cf1440ccd497e24c1871267c9e2982e
Trevin <trex.trevin@gmail.com>
2025-04-22 13:09:57 -0500

    updated servo logic

--- ENY/ENY.ino
+++ ENY/ENY.ino
@@ -12,22 +12,39 @@
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
 
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
 #define BMP_SCK 13
 #define BMP_MISO 12
 #define BMP_MOSI 11
 #define BMP_CS 10
 
 #define SEALEVELPRESSURE_HPA (1013.25)
-// float baseAlt = 0.0;
-// float lastAlt = 0.0;
 
-// const int trigPin = 9;
-// const int echoPin = 10;
+#define rate 60 // speed of the servo
+#define released  0 // released camera
+#define pressed  60 // pressign camera
+#define trigger  0.25 // 0.25 m/s is variability of pressure reading
 
+
+// pressure variables
 Adafruit_BMP3XX bmp;
+// float baseAlt = 0.0;
+float notBaseAlt = 0.0;
+float lastAlt = 0.0;
+
+// servo variables
+bool camera = true;
+float angle = 0;
+int direction = 1;
+
+
+
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(9600);
     while (!Serial);
     Serial.println("Adafruit BMP388 / BMP390 test");
 
@@ -48,14 +65,37 @@ void setup() {
     // \bmp388
 
     // button
-    pinMode(2, INPUT_PULLUP);
+    // pinMode(2, INPUT_PULLUP);
 
     // \button
+    // signal out
+    pinMode(10, OUTPUT);
+    // \signal out
 
     // USR
     // pinMode(trigPin, OUTPUT);
     // pinMode(echoPin, INPUT);
     // \USR
+
+    // ignoring the first 2 readings
+    myservo.attach(3);
+    notBaseAlt = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+    Serial.print("not base altitude = ");
+    Serial.print(notBaseAlt);
+    Serial.println(" m");
+    delay(1000);
+    notBaseAlt = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+    Serial.print("probably not base altitude = ");
+    Serial.print(notBaseAlt);
+    Serial.println(" m");
+    delay(1000);
+    // myservo.write(released);
+
+    // the altitude at the ground
+    notBaseAlt = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+    Serial.print("base altitude = ");
+    Serial.print(notBaseAlt);
+    Serial.println(" m");
 
 
 }
@@ -67,50 +107,71 @@ void loop() {
 
     if (! bmp.performReading()) {
         Serial.println("Failed to perform reading :(");
-        return;
+        // return;
     }
 
-
-    Serial.print("Temperature = ");
-    Serial.print(bmp.temperature);
-    Serial.println(" *C");
 
     Serial.print("Pressure = ");
     Serial.print(bmp.pressure / 100.0);
     Serial.println(" hPa");
 
 
-    if (digitalRead(2) == LOW) {
-        baseAlt = bmp.readAltitude(SEALEVELPRESSURE_HPA);
-        Serial.println("Ground altitude set!");
-    }
-
+    float seaAlt = bmp.readAltitude(SEALEVELPRESSURE_HPA);
     Serial.print("sea Altitude = ");
-    Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.print(seaAlt);
     Serial.println(" m");
 
-    float groundAlt = bmp.readAltitude(SEALEVELPRESSURE_HPA)- baseAlt;
+    // if (digitalRead(2) == LOW) {
+    // if (first) {
+    //     first = false;
+    //     notBaseAlt = seaAlt;
+    //     Serial.println("Ground altitude set!");
+    // }
+
+
+    float groundAlt = seaAlt - notBaseAlt;
     Serial.print("ground altitude = ");
     Serial.print(groundAlt);
     Serial.println(" m");
 
     // vertical speed
-    float verticalSpeed = (groundAlt - lastAlt)/2;
+    float verticalSpeed = (groundAlt - lastAlt)/.500;
     Serial.print("Vertical velocity = ");
     Serial.print(verticalSpeed);
     Serial.println(" m/s");
     lastAlt = groundAlt;
-    // if (groundAlt < 1 && abs(verticalSpeed)<1){// basically on ground
-    // 
-    // 
-    // } 
+    
 
-    // if (groundAlt > 30 && abs(verticalSpeed)<1){// top altitude
-    // 
-    // 
-    // } 
+    // logic for shuttering the camera
+    if (abs(verticalSpeed)<trigger){
+        // basically at rest (pressure changes relatively slowly)
+        digitalWrite(10, LOW);
+        camera = false;
+    // } else if (groundAlt > 1 && verticalSpeed < trigger){
+    //     // basically falling (pressure increases when pressure is above the base pressure)
+    } else if (verticalSpeed < trigger){
+        // basically falling (pressure increases)
+        camera = true;
+        digitalWrite(10, HIGH);
+    } else{  
+        // going up, falling below 1m
+        camera = false;  
+        digitalWrite(10, LOW);
+    }
 
 
-    Serial.println();
-    delay(2000);
+    // press the camera shutter button
+    myservo.write(released);
+    if (camera){
+        angle += direction * rate;
+        if (angle > pressed || angle < released ){
+            direction *= -1;
+            angle += direction * rate;
+        }
+        myservo.write(angle);
+    }
+
+
+    Serial.println("---------------");
+    delay(500);
 }
